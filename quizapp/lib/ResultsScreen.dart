69ff71715c810d927a0e4c7ca73bde923a9dad9d
Laksh <lakshd1999@gmail.com>
2025-01-19 02:18:08 +0530

    Created the project

---
+++ quizapp/lib/ResultsScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Resultsscreen extends StatefulWidget {
+  const Resultsscreen({super.key});
+
+  @override
+  State<Resultsscreen> createState() => _ResultsscreenState();
+}
+
+class _ResultsscreenState extends State<Resultsscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ quizapp/lib/StartQuizScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Startquizscreen extends StatefulWidget {
+  const Startquizscreen({super.key});
+
+  @override
+  State<Startquizscreen> createState() => _StartquizscreenState();
+}
+
+class _StartquizscreenState extends State<Startquizscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
